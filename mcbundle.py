dc2f2ce3f2d4a87ad0809b5030122d95670cdfd6
Evan Bishop <evanpbishop@gmail.com>
2020-11-13 20:13:22 -0500

    Added an optional argument in function array_to_bundle_data

--- mcbundle.py
+++ mcbundle.py
@@ -1,10 +1,10 @@
 def text_to_item(text):
-	return f"""{"{"}id: "minecraft:{text}", Count: 1{"}"}"""
+	return f"""{"{"}id:"minecraft:{text}",Count:1{"}"}"""
 #end
-def array_to_bundle_data(array):
+def array_to_bundle_data(array, selector="@p"):
 	full_array = []
 	for i in array:
 		full_array.append(text_to_item(i))
 	full_text = ",".join(full_array)
-	return f"""give @p bundle{"{"}Items: [{full_text}]{"}"}"""
+	return f"""give {selector} bundle{"{"}Items:[{full_text}]{"}"}"""
 #end
