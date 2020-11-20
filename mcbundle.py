8cae808aee9dc85f47c3801784357df69a8505d5
Evan Bishop <evanpbishop@gmail.com>
2020-11-20 17:04:07 -0500

    Added more customization options

--- mcbundle.py
+++ mcbundle.py
@@ -1,10 +1,10 @@
 def text_to_item(text):
 	return f"""{"{"}id:"minecraft:{text}",Count:1{"}"}"""
 #end
-def array_to_bundle_data(array, selector="@p"):
+def array_to_bundle_data(array, prefix="give @p bundle"):
 	full_array = []
 	for i in array:
 		full_array.append(text_to_item(i))
 	full_text = ",".join(full_array)
-	return f"""give {selector} bundle{"{"}Items:[{full_text}]{"}"}"""
+	return f"""{prefix}{"{"}Items:[{full_text}]{"}"}"""
 #end
